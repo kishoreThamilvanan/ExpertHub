25126aa2f7baf3e199e2576ef585da06fcf84d6d
Rongan Li <rongan.li@stonybrook.edu>
2018-06-13 15:43:04 -0400

    added expert page and tryinng to connect to mySQL

--- apprunner.py
+++ apprunner.py
@@ -1,5 +1,5 @@
 from flask import *
-
+from flaskext.mysql import MySQL
 
 app = Flask(__name__)
 
@@ -9,16 +9,26 @@ def index():
     return render_template("index.html")
 
 
-
-
-
-
+@app.route('/experts/')
+def route1():
+    return render_template('experts.html')
 
 
 if __name__ == "__main__":
-   app.run(debug=True)
+    app.run(debug=True)
 
+mysql = MySQL()
 
+dict = {}
 
+# MySQL configurations
+app.config['MYSQL_DATABASE_USER'] = 'root'
+app.config['MYSQL_DATABASE_PASSWORD'] = 'iLove$100only'
+app.config['MYSQL_DATABASE_DB'] = 'expertHub'
+app.config['MYSQL_DATABASE_HOST'] = 'localhost'
+mysql.init_app(app)
 
+conn = mysql.connect()
 
+review = mysql.connect().cursor()
+review.execute("SELECT * FROM clients")
