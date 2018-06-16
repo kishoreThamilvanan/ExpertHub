4c1981c9dee9f97cbd1f2acd1a1d09d3ec1d80a7
Rongan Li <rongan.li@stonybrook.edu>
2018-06-16 00:01:18 -0400

    finish the layout of the expert page, but not the functionality asscociated with database yet.

--- apprunner.py
+++ apprunner.py
@@ -4,24 +4,7 @@ from flaskext.mysql import MySQL
 app = Flask(__name__)
 
 
-@app.route("/")
-def index():
-    return render_template("index.html")
-
-
-@app.route('/experts/')
-def route1():
-    return render_template('experts.html')
-
-
-if __name__ == "__main__":
-    app.run(debug=True)
-
 mysql = MySQL()
-
-dict = {}
-
-# MySQL configurations
 app.config['MYSQL_DATABASE_USER'] = 'root'
 app.config['MYSQL_DATABASE_PASSWORD'] = 'iLove$100only'
 app.config['MYSQL_DATABASE_DB'] = 'expertHub'
@@ -29,6 +12,23 @@ app.config['MYSQL_DATABASE_HOST'] = 'localhost'
 mysql.init_app(app)
 
 conn = mysql.connect()
+cursor =conn.cursor()
 
-review = mysql.connect().cursor()
-review.execute("SELECT * FROM clients")
+cursor.execute("SELECT * from clients")
+data = cursor.fetchall()
+
+
+
+@app.route("/")
+def index():
+    return render_template("index.html")
+
+
+@app.route('/experts')
+def route1():
+    return render_template("experts.html", data=data)
+
+
+if __name__ == "__main__":
+    app.run(debug=True)
+
